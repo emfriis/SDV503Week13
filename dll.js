c04598dd81c867503a178d470278eb91e1d4f510
Emily Friis <emily-friis@live.nmit.ac.nz>
2020-06-02 10:39:56 +1200

    Added linked list methods

--- dll.js
+++ dll.js
@@ -1,16 +1,16 @@
 class Node {
     constructor(data) {
-        this.data = data;
-        this.prev = null;
-        this.next = null;
+        this.data = data; // Sets new node's data to input value.
+        this.prev = null; // Sets new node's previous node to null.
+        this.next = null; // Sets new node's next node to null.
     };
 };
 
 class DoublyLinkedList {
     constructor() {
-        this.length = 0;
-        this.head = null;
-        this.tail = null;
+        this.length = 0; // Sets length to 0.
+        this.head = null; // Sets head to null.
+        this.tail = null; // Sets tail to null.
     };
 
     addAtTail(data) {
@@ -55,13 +55,35 @@ class DoublyLinkedList {
         this.length-- // Decrements length.
     };
 
+    removeAtIndex(index) {
+        if (this.head === null || index < 0) { // Executes if there are no nodes in list or the index is negative.
+            return undefined;
+        };
+        let current = this.head; // Sets current node to head.
+        let counter = 0; // Sets counter to 0.
+        while (current !== null && counter < index) { // While the current node isn't null and the counter is less than the index.
+            current = current.next; // Sets the current node to the next node.
+            counter++; // Increments the counter.
+        };
+        if (current !== null) { // Executes if node was found.
+            current.previous.next = current.next; // Points current's previous node's next node to current's next node.
+            if (this.tail === current) { // Executes if current is the current tail.
+                this.tail = current.previous; // Sets current's previous node to new tail.
+            } else {
+                current.next.previous = current.previous; // Points current's next node's previous node to current's previous node.
+            };
+        } else {
+            return undefined;
+        };
+    };
+
     getAtIndex(index) {
         if (index > -1) { // Executes if index is positive;
             let current = this.head; // Sets current node to head.
-            let i = 0; // Sets counter to 0;
-            while(current !== null && i < index) { // While the current node isn't null and the counter is less than the index.
-                current = current.next; // Sets the current node to the next node;
-                i++; // Increments the counter;
+            let counter = 0; // Sets counter to 0;
+            while(current !== null && counter < index) { // While the current node isn't null and the counter is less than the index.
+                current = current.next; // Sets the current node to the next node.
+                counter++; // Increments the counter.
             };
             return current !== null ? current.data : undefined; // If current node isn't null returns its data, if not, returns undefined.
         } else {
--- index.js
+++ index.js
@@ -45,6 +45,30 @@ class SinglyLinkedList {
 
     };
 
+    get(index) {
+        if (inbdex < 0 || index >= this.length) {
+            return null;
+        };
+        var counter = 0;
+        var current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter ++;
+        };
+        return current;
+    };
+
+    set(index, val) {
+        var foundNode = this.get(index);
+        if(foundNode) {
+            foundNode.val = val;
+            return true;
+        };
+        return false;
+    };
+
+    
+
 };
 
 var list = new SinglyLinkedList();
